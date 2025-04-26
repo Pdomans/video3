54a011e6974e5d1770bd15c5018c6aa04d0c8d94
pdomans <pablodomanski90@gmail.com>
2025-04-25 23:51:10 -0300

    se creo  ejercicio 2 y se agrego una linea al ejercicio 1

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,4 @@
-nombre="pablo"
-apellido="doma"
+nombre=input("ingrese su nombre")
+apellido=input("ingrese su nombre")
 print (f"hola {nombre} como estas tu apellido es ? {apellido}") 
 materia="programacion 1"
-print("estamos en la rama 2 papa!")
