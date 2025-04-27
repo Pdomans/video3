9414d4913aa60dbd76800740e3d45de36629dd5c
pdomans <pablodomanski90@gmail.com>
2025-04-27 01:28:09 -0300

    se agregaron comentarios a ejercicio 1

--- eejecicio1.py
+++ eejecicio1.py
@@ -1,1 +1,9 @@
-c
+nombre=input("ingrese su nombre")
+#se define varialbe nombre 
+apellido=input("ingrese su nombre")
+#se define varialbe apellido
+print (f"hola {nombre} como estas tu apellido es ? {apellido}") 
+#se imprime mensaje 
+materia="programacion 1"
+#se define variable materia 
+
